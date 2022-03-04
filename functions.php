748841f58d4171ba9b86789a7d6c9580a0cb7d57
AlexanderEvanzu <alexander@evanzu.com>
2022-03-04 00:53:44 -0500

    first commit

---
+++ functions.php
@@ -0,0 +1,153 @@
+
+<?php
+
+// Incluir Bootstrap CSS
+function bootstrap_css()
+{
+	wp_enqueue_style(
+		'bootstrap_css',
+		'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
+		array(),
+		'4.5.2'
+	);
+}
+add_action('wp_enqueue_scripts', 'bootstrap_css');
+
+
+// Incluir Bootstrap JS y dependencia popper
+function enqueue_load_bootstrap() {
+
+    // Add popper js
+    wp_register_script( 'popper-js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', ['jquery'], NULL, true );
+    wp_enqueue_script( 'popper-js' );
+
+    // Add bootstrap js
+    wp_register_script( 'bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', ['jquery'], NULL, true );
+    wp_enqueue_script( 'bootstrap-js' );
+}
+
+add_action( 'wp_enqueue_scripts', 'enqueue_load_bootstrap' );
+
+
+add_action( 'wp_enqueue_scripts', 'enqueue_load_fa' );
+function enqueue_load_fa() {
+wp_enqueue_style( 'load-fa', 'https://use.fontawesome.com/releases/v5.3.1/css/all.css' );
+}
+
+//llamar al archivo de custimización
+require_once(trailingslashit(get_template_directory()) . 'inc/customize.php');
+
+
+
+
+//clase css
+
+add_filter('nav_menu_css_class', 'raiola_nav_class', 10, 2);
+
+function raiola_nav_class($classes, $item)
+{
+	$classes[] = 'nav-link';
+	return $classes;
+}
+
+// css personalizado
+add_action('wp_enqueue_scripts', 'custom_enqueue_styles');
+
+function custom_enqueue_styles()
+{
+	wp_enqueue_style(
+		'custom-style',
+		get_stylesheet_directory_uri() . '/assets/css/custom.css',
+		array(),
+		wp_get_theme()->get('Version')
+	);
+}
+
+// Mansonry
+
+function bps_masonry()
+{
+	wp_enqueue_script('masonry-js', '//unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js', array('jquery'));
+	wp_enqueue_script('masonry-init', get_stylesheet_directory_uri() . '/assets/js/masonry-init.js', array('masonry'), 1, true);
+}
+
+add_action('wp_enqueue_scripts', 'bps_masonry');
+
+
+add_theme_support('post-thumbnails');
+function setup_types()
+{
+	register_post_type('mytype', array(
+		'label' => __('My type'),
+		'supports' => array('title', 'editor', 'thumbnail', 'revisions'),
+		'show_ui' => true,
+	));
+}
+add_action('init', 'setup_types');
+
+
+//custom post type 
+function team()
+{
+	$labels = array(
+		'name'    => _x('Team evanzu', 'travel-booking'),
+		'singular_name'  => _x('url integrante', 'travel-booking'),
+		'alojamiento_name' => _x('Slider', 'travel-booking'),
+		'name_admin_bar' => _x('Slider', 'travel-booking'),
+		'add_new'   => _x('Añadir nuevo integrante', 'travel-booking'),
+		'add_new_item'  => __('Añadir nuevo integrante', 'travel-booking'),
+		'new_item'   => __('Imagenes', 'travel-booking'),
+		'edit_item'   => __('Editar Imagen', 'travel-booking'),
+		'view_item'   => __('Ver Imagen', 'travel-booking'),
+		'all_items'   => __('Listado equipo evanzu', 'travel-booking'),
+		'search_items'  => __('Buscar Imagenes', 'travel-booking'),
+		'parent_item_colon' => __('Parent Intercambio', 'travel-booking'),
+		'not_found'   => __('No se han encontrado intercambios de alojamiento', 'travel-booking'),
+		'not_found_in_trash' => __('No se han encontrado intercambio de alojamiento en la papelera', 'travel-booking')
+	);
+	$args = array(
+		'labels'   => $labels,
+		'description'  => __('Description', 'travel-booking'),
+		'public'   => true,
+		'publicly_queryable' => true,
+		'show_ui'   => true,
+		'show_in_menu'  => true,
+		'query_bar'   => true,
+		'rewrite'   => array('slug' => 'Imagen'),
+		'capability_type' => 'post',
+		'has_archive'  => true,
+		'hierarchical'  => false,
+		'menu_position' => 5,
+		'menu_icon'    => 'dashicons-id-alt',
+		'supports'   => array('slug' => 'title', 'editor', 'thumbnail'),
+		'rewrite'   => array('post_tag'),
+	);
+	register_post_type('team', $args);
+}
+add_action('init', 'team', 4);
+
+
+//menu
+
+add_action('after_setup_theme', 'raiola_registrar_menu');
+function raiola_registrar_menu()
+{
+	register_nav_menu('menu-principal', 'Menu Principal');
+}
+
+
+// nuevos menus /////
+
+if (!function_exists('marabelia_menus')) { // Register Navigation Menus
+	function marabelia_menus()
+	{
+		$locations = array(
+			'footer' => __('Menu ubicado en el footer', 'text_domain'),
+			'redes-sociales' => __('links redes sociales en el footer', 'text_domain'),
+			'servicios' => __('links servicios en el footer', 'text_domain'),
+		);
+		register_nav_menus($locations);
+	}
+	add_action('init', 'marabelia_menus');
+}
+?>
---
+++ pages/marqueting_digital.php
@@ -0,0 +1,396 @@
+<?php
+/* 
+    Template Name: Marketing
+*/
+
+get_header();
+
+?>
+
+<style>
+     .gallery-wrapper {
+  overflow: hidden;
+}
+
+.grid-item {
+  padding-bottom: 3rem;
+}
+
+.sidebar {
+  text-align: center;
+  padding: 1rem;
+  background: red;
+  height: 900px;
+} 
+</style>
+
+<section id="slider">
+
+    <div id="demo" class="carousel slide" data-ride="carousel">
+
+        <!-- Indicators -->
+        <ul class="carousel-indicators">
+            <li data-target="#demo" data-slide-to="0" class="active"></li>
+            <li data-target="#demo" data-slide-to="1"></li>
+            <li data-target="#demo" data-slide-to="2"></li>
+
+        </ul>
+
+        <div class="float-rectangle-banner">
+            <div id="rectangle" class="text-center">
+            </div>
+        </div>
+
+        <!-- The slideshow -->
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="<?php echo get_option("evanzu")["carrusell-image-one"]; ?>">
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo get_option("evanzu")["carrusell-image-two"]; ?>">
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo get_option("evanzu")["carrusell-image-one"]; ?>">
+            </div>
+
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#demo" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </a>
+
+    </div>
+
+</section>
+<section>
+    <div class="bg-white">
+        <div class="mx-auto w-50">
+            <ul class="nav nav-pills" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" data-toggle="pill" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-toggle="pill" href="#menu1">Menu 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-toggle="pill" href="#menu2">Menu 2</a>
+                </li>
+            </ul>
+
+        </div>
+
+        <!-- Tab panes -->
+        <div class="tab-content">
+            <div id="home" class="tab-pane active"><br>
+               <div class="bg-faint-gray">
+                <div class="big-title text-red my-4 text-center">
+                    Publicidad Digital
+                </div>
+                <div class="container">
+                    <div class="row">
+                          <div class="col-sm-6">
+                            <img src="<?php echo get_template_directory_uri() ?>/assets/images/cellphone-publicidad-digital.svg" alt="">
+                            <div class="text-red text-center my-3">
+                                 Ideal para promocionar
+                            </div>
+                            <p class="text-justify">
+                            Ventas, tráfico web, consideración de marca, productos, servicios, lanzamientos, ofertas, aplicaciones móviles y promociones. Sea cual sea tu objetivo publicitario (visitas a tu web, vistas de videos, conversiones, descargas de aplicaciones, visitas o llamadas a tu negocio físico, etc) te ayudaremos a alcanzarlo a través de los diversos formatos de publicidad.
+                            </p>
+                            <p class="text-justify">
+                            Da el primer paso para promocionar tus productos.
+                            </p>
+
+                            
+                          </div>
+                          <div class="col-sm-6">
+
+                            <div class="my-3">
+                            Atrae a tu próximo comprador con publicidad en canales digitales.
+                            </div>
+                              <p class="text-justify">
+                                   Atrae a tu próximo comprador con publicidad en canales digitales.
+                              </p>
+                              <div class="text-red my-1">
+                                   Las plataformas digitales que manejamos:        
+                              </div>
+                              <p>
+                                  . <strong>Google Adwords</strong> (Búsqueda, Display, Youtube, Shopping y App móviles).
+                              </p>
+                              <p>
+                                  . <strong>Publicidad en plataformas sociales</strong> (Facebook, Instagram, Twitter, Linkedin, TikTok, Wase, Spotify, Shazam, y otros).
+                              </p>
+
+                              <img src="<?php echo get_template_directory_uri() ?>/assets/images/laptop-publicidad-digital.svg" alt="">
+
+                          </div>
+                    </div>
+                </div>
+                </div>
+                <div>
+                     <div class="big-title text-red my-4 text-center">
+                       El servicio incluye:
+                     </div>
+                     <div>
+                         <div class="d-flex justify-content-center">
+                             <div>  
+                                 <div class="mb-1">
+                                     <div class="d-inline-block"><img src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/first-icon-service.svg"></div>
+                                          <div class="d-inline-block ml-1">
+                                              Plan de medios digitales <br>
+                                               (definir los canales con los  <br> que tu público interactúa)
+                                        </div>   
+                                   </div> 
+                                    <div class="ml-5">
+                                        <img  class="pl-2" src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/ponit-line-red.svg">
+                                    </div>
+                                   
+                             </div>
+                             <div> 
+                                <div> 
+                                    <svg width="10" height="114" viewBox="0 0 7 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                      <path d="M0 3.5C0 1.567 1.567 0 3.5 0C5.433 0 7 1.567 7 3.5V52.5C7 54.433 5.433 56 3.5 56C1.567 56 0 54.433 0 52.5V3.5Z" fill="#ED1164"/>
+                                    </svg>
+                                 </div> 
+                                 <div>
+                                     <svg width="19" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="9" cy="9.5" r="9" fill="#ED1164"/>
+                                      </svg>
+                                 </div>
+                                 <div>
+                                      <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle r="9" transform="matrix(-1 0 0 1 9 9.5)" fill="#FBC000"/>
+                                      </svg>
+                                 </div>
+                              </div>
+                             <div>
+                                   <div class="mb-1 mt-5">         
+                                          <div class="d-inline-block ml-1">
+                                            Definir el <strong>formato </strong> <br> de <strong>publicidad</strong> adecuado.
+                                        </div> 
+                                          <div class="d-inline-block ml-4"><img src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/secon-icon-righ.svg"></div>  
+                                   </div> 
+
+                                 <div >
+                                     <svg width="290" height="23" viewBox="0 0 290 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M0.999998 24.5L288 24.5C288.552 24.5 289 24.0523 289 23.5L289 1" stroke="#FBC000" stroke-width="2" stroke-linecap="round" stroke-dasharray="6 6"/>
+                                        </svg>
+                                 </div>
+                             </div>
+                         </div>
+
+                        <div class="d-flex justify-content-center">
+                            <div>  
+                                <div class="mb-1">
+                                    <div class="d-inline-block"><img src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/contain-define.svg"></div>
+                                        <div class="d-inline-block ml-1">
+                                        <strong> Definición de contenidos <br> publicitarios</strong>  (Mensaje y <br> gráfica o audiovisual).
+                                    </div>   
+                                </div> 
+                                <div class="ml-5">
+                                    <img  class="pl-2" src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/ponit-line-red.svg">
+                                </div>
+                                
+                            </div>
+                            <div> 
+                            <div> 
+                                <svg width="10" height="114" viewBox="0 0 7 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0 3.5C0 1.567 1.567 0 3.5 0C5.433 0 7 1.567 7 3.5V52.5C7 54.433 5.433 56 3.5 56C1.567 56 0 54.433 0 52.5V3.5Z" fill="#ED1164"/>
+                                </svg>
+                                </div> 
+                                <div>
+                                    <svg width="19" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="9" cy="9.5" r="9" fill="#ED1164"/>
+                                    </svg>
+                                </div>
+                                <div>
+                                    <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle r="9" transform="matrix(-1 0 0 1 9 9.5)" fill="#FBC000"/>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="mb-1 mt-3">         
+                                        <div class="d-inline-block ml-1">
+                                        <strong>Despliegue y configuración <br> de la campaña en la </strong>  <br>plataforma seleccionada.
+                                    </div> 
+                                        <div class="d-inline-block"><img src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/deploy-configuration.svg"></div>  
+                                </div> 
+
+                                <div >
+                                    <svg width="290" height="24" viewBox="0 0 290 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0.999998 24.5L288 24.5C288.552 24.5 289 24.0523 289 23.5L289 1" stroke="#FBC000" stroke-width="2" stroke-linecap="round" stroke-dasharray="6 6"/>
+                                    </svg>
+                                </div>
+                            </div>
+
+                            
+
+
+                        </div>
+
+                        <div class="d-flex justify-content-center">
+                            <div>  
+                                <div class="mb-1">
+                                    <div class="d-inline-block"><img src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/rocket-optimisation.svg"></div>
+                                        <div class="d-inline-block ml-2">
+                                                <strong>Optimización</strong>  de la <br>campaña. 
+                                        </div>   
+                                </div> 
+                                    <div class="ml-5">
+                                        <img  class="pl-2" src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/ponit-line-red.svg">
+                                    </div>
+                                
+                            </div>
+                            <div> 
+                                <div> 
+                                    <svg width="10" height="114" viewBox="0 0 7 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0 3.5C0 1.567 1.567 0 3.5 0C5.433 0 7 1.567 7 3.5V52.5C7 54.433 5.433 56 3.5 56C1.567 56 0 54.433 0 52.5V3.5Z" fill="#ED1164"/>
+                                    </svg>
+                                </div> 
+                                <div>
+                                    <svg width="19" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle cx="9" cy="9.5" r="9" fill="#ED1164"/>
+                                    </svg>
+                                </div>
+                                <div>
+                                    <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <circle r="9" transform="matrix(-1 0 0 1 9 9.5)" fill="#FBC000"/>
+                                    </svg>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="ml-5 mt-5">         
+                                        <div class="d-inline-block ml-1 mr-5">
+                                        <strong>Reportes <br> periódicos.</strong> 
+                                        </div> 
+                                        <div class="d-inline-block ml-5"><img src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/periodic-reports.svg"></div>  
+                                </div> 
+
+                                <div >
+                                    <svg width="290" height="24" viewBox="0 0 290 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M0.999998 24.5L288 24.5C288.552 24.5 289 24.0523 289 23.5L289 1" stroke="#FBC000" stroke-width="2" stroke-linecap="round" stroke-dasharray="6 6"/>
+                                        </svg>
+                                </div>
+                            </div>
+
+                        </div>
+                         
+                         <div class="d-flex justify-content-center">
+                               
+                              <div class="d-inline-block ml-5"><img src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/root-tree.svg"></div>  
+                        </div>
+                     </div>
+                </div>
+                <div class="bg-red">
+                           <div class="bg-pink-form rounded">
+                               <div class="big-title text-white text-center"> Define tu presupuesto</div>
+
+                                 <div class="bg-white mx-5 px-5 rounded-pill">
+                                      <div class="d-inline-block w-75"> 
+                                            <div class="input-group">
+                                           
+                                            <input type="text" placeholder="Sitio Web" class="form-control">
+                                            <input type="text" placeholder="E-mail" class="form-control">
+                                            </div>
+                                      </div>
+                                      <div class="d-inline-block"><button type="button" class="btn bg-red rounded-pill text-white my-1 p-2 btn-sm">Solicita tu cotización</button> </div>
+                                 </div>
+           
+                            </div>   
+                </div>
+            </div>
+            <div id="menu1" class="container tab-pane fade"><br>
+                <h3>Menu 1</h3>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <div id="menu2" class="container tab-pane fade"><br>
+                <h3>Menu 2</h3>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+            </div>
+        </div>
+    </div>
+
+</section>
+<section>
+      <div class="big-title text-red my-4 text-center">
+         PORTAFOLIO
+      </div> 
+        <div>
+                 <div class="container" style="margin-top:5rem">
+  <div class="row">
+    
+    <div class="col-lg-8">
+      <div class="gallery-wrapper clearfix">
+        <div class="col-lg-6 grid-sizer">  </div>
+        <div class="col-lg-6 grid-item">
+            primer parte
+          <img class="img-fluid" src="http://via.placeholder.com/1024x900">
+        </div>
+
+        <div class="col-lg-6 grid-item">
+            segunda parte
+          <img class="img-fluid" src="http://via.placeholder.com/800x370">
+        </div>
+
+        <div class="col-lg-6 grid-item">
+          <img class="img-fluid" src="http://via.placeholder.com/500x370">
+        </div>
+
+        <div class="col-lg-6 grid-item">
+          <img class="img-fluid" src="http://via.placeholder.com/1024x600">
+        </div>
+
+        <div class="col-lg-6 grid-item">
+          <img class="img-fluid" src="http://via.placeholder.com/1024x300">
+        </div>
+
+        <div class="col-lg-6 grid-item">
+          <img class="img-fluid" src="http://via.placeholder.com/1024x600">
+        </div>
+
+        <div class="col-lg-6 grid-item">
+          <img class="img-fluid" src="http://via.placeholder.com/800x900">
+        </div>
+
+        <div class="col-lg-6 grid-item">
+          <img class="img-fluid" src="http://via.placeholder.com/1024x600">
+        </div>
+
+      </div>
+    </div>
+
+    <div class="col-lg-4 sidebar">
+      <h3>Sidebar</h3>
+    </div>
+    
+    
+  </div>
+</div>
+        </div>
+
+      
+</section>
+
+
+ <script>
+     jQuery(function ($) {
+	
+    var $grid = $('.gallery-wrapper').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true,
+    transitionDuration: 0,
+  });
+
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry();
+  });
+
+    });
+  </script>
+
+<?php get_footer(); ?>
