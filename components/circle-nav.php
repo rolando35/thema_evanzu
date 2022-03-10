370ac7b1f2e99e1d47d57e2eb3681c003bd6b8bd
rolando <rolando@evanzu.com>
2022-03-09 19:06:45 -0500

    first circle components

---
+++ components/circle-nav.php
@@ -0,0 +1,13 @@
+<div  class='circle-nav'>
+    <?php 
+       global $data; 
+       echo $data['svg'];    
+       ?>   
+</div>
+<div class="title-circle-nav text-gray w-75">
+     <?php 
+       echo $data['title'];    
+       ?>  
+</div>
+
+
