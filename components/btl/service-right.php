8b22fbc99f6a6c79518d1d558b43ea861b7fae7e
programador <programador@MacBook-Air-de-programador.local>
2022-03-23 13:46:38 -0500

    Implement service component to btl module

--- components/btl/service-right.php
+++ components/btl/service-right.php
@@ -1,3 +1,4 @@
+<?php $width_image = $arg['image'] == '' ? '96px' : 'auto' ?>
 <div class="d-flex flex-column my-1 my-lg-0 align-self-lg-end">
 	<div class="d-flex flex-column-reverse align-items-center flex-lg-row">
 		<div
@@ -5,7 +6,7 @@
 		>
         <?php echo $args["text"]; ?>
 		</div>
-		<div class="align-self-lg-end">
+		<div class="align-self-lg-end" style="width: <?php echo $width_image; ?>" >
 			<img class="image-service" src="<?php echo $args["image"]; ?>"
 			/>
 		</div>
