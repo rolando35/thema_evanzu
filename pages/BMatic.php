b31ba389c46a5581d9cf71cec1a5b6320fd6af2c
AlexanderEvanzu <alexander@evanzu.com>
2022-03-24 12:28:09 -0500

    add Our Clients

---
+++ pages/BMatic.php
@@ -0,0 +1,222 @@
+<?php 
+/* 
+    Template Name: BMATIC
+*/
+get_header();
+
+$path_img = get_template_directory_uri() . '/assets/images/BMatic/';
+?>
+
+<section class="pt-5 pb-3">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-6 text-center text-md-left">
+				<img
+					src="<?php echo $path_img?>title.svg"
+					class="img-fluid px-1 py-3"
+				/>
+				<h1 class="px-1 py-2 text-light-gray title-client text-uppercase">
+					Gestor de colas online
+				</h1>
+			</div>
+			<div class="col-md-6">
+				<div class="p-2 text-light-gray text-main-client">
+					BMatic ayuda a las empresas a gestionar las colas y así evitando la acumulación de personas. Un gestor de colas intuitivo y de fácil uso.
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="bg-light-gray pt-3 pb-5">
+	<div class="container">
+		<!---------------title--------------->
+		<div class="container-subtitle-client">
+			<h2 class="text-red text-center subtitle-client p-1 m-0">
+                Diseño de identificador gráfico
+            </h2>
+
+			<p class="text-light-gray text-center p-1 m-0 text-secondary-client">
+				Diseñamos un simbolo para posicionar la marca, con unos colores vibrantes que resalten a la marca.
+			</p>
+		</div>
+		<!---------------title--------------->
+
+		<!---------------images-------------->
+		<div class="row">
+			<div class="col-md-5 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Bmatic-antes-a.jpg" class="w-100 h-100" />
+			</div>
+			<div class="d-none col-md-2 d-md-flex align-items-md-center">
+				<div class="w-100 px-1">
+					<img src="<?php echo $path_img?>flecha.svg" class="w-100" />
+				</div>
+			</div>
+			<div class="col-md-5 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Bmatic-despues-a.jpg" class="w-100 h-100" />
+			</div>                        
+			<div class="col-12 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Manual-BMatic-Evanzu.jpg" class="w-100 h-100" />
+			</div>
+        </div>
+        <!---------------images-------------->
+
+
+        <!---------------title--------------->
+		<div class="container-subtitle-client">
+			<h2 class="text-red text-center subtitle-client p-1 m-0">
+                Diseño UX / UI
+            </h2>
+
+			<p class="text-light-gray text-center p-1 m-0 text-secondary-client">
+				Diseñamos la experiencia de usuario optimo con un diseño atráctivo que permita captar leads, realizamos la programación de su web para estar optimo y amigable con Google.
+			</p>
+		</div>
+		<!---------------title--------------->
+
+		<!---------------images-------------->
+		<div class="row">
+			<div class="col-md-6 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>web-bmatic-portafolio.jpg" class="w-100 h-100" />
+			</div>
+			<div class="col-md-6 p-0 m-0">
+                <div class="d-flex flex-column h-100 w-100">
+                    <div class="w-100 h-50 p-1 m-0 p-md-2">
+                        <img src="<?php echo $path_img?>app-bmatic-portafolio.jpg" class="w-100 h-100" />
+                    </div>                        
+                    <div class="w-100 h-50 p-1 m-0 p-md-2">
+                        <img src="<?php echo $path_img?>Web-planes-BMatic.jpg" class="w-100 h-100" />
+                    </div>
+                </div>
+			</div>
+        </div>
+        <!---------------images-------------->
+
+        <!---------------title--------------->
+		<div class="container-subtitle-client">
+			<h2 class="text-red text-center subtitle-client p-1 m-0">
+                Elaboración de video 2D
+            </h2>
+
+			<p class="text-light-gray text-center p-1 m-0 text-secondary-client">
+				Creamos el video de impulso y de comunicación de servicio del sistema de BMatic, generando los gráficos y animación.
+			</p>
+		</div>
+		<!---------------title--------------->
+
+		<!---------------video-------------->
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="video-responsive">
+                    <iframe
+                    height="215"
+                    src="https://www.youtube.com/embed/zpq6oje5FsA?controls=0"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                    ></iframe>
+                </div>
+            </div>
+        </div>
+        <!---------------video-------------->
+
+
+        <!---------------title--------------->
+		<div class="container-subtitle-client">
+			<h2 class="text-red text-center subtitle-client p-1 m-0">
+                Diseño de contenido para redes sociales
+            </h2>
+
+			<p class="text-light-gray text-center p-1 m-0 text-secondary-client">
+				Generamos el contenido y diseño para el lanzamiento del servicio de agendamiento de citas, para las plataformas de Facebook, Instagram y Twitter.
+			</p>
+		</div>
+		<!---------------title--------------->
+
+		<!---------------images-------------->
+		<div class="row">
+			<div class="col-12 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>redes-sociales.jpg" class="w-100 h-100" />
+			</div>
+            <div class="col-8 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Redes-BMatic-A.jpg" class="w-100 h-100" />
+			</div>
+            <div class="col-4 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Redes-BMatic.jpg" class="w-100 h-100" />
+			</div>
+        </div>
+        <!---------------images-------------->
+
+
+        <!---------------title--------------->
+		<div class="container-subtitle-client">
+			<h2 class="text-red text-center subtitle-client p-1 m-0">
+                Estrategia de Adwords
+            </h2>
+
+			<p class="text-light-gray text-center p-1 m-0 text-secondary-client">
+				Realizamos la planificación estratégica en Google Ads, para llegar al público a nivel nacional.
+			</p>
+		</div>
+		<!---------------title--------------->
+
+		<!---------------images-------------->
+		<div class="row">
+			<div class="col-6 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Adwords-BMatic.jpg" class="w-100 h-100" />
+			</div>
+            <div class="col-6 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Adwords-BMatic-2.jpg" class="w-100 h-100" />
+			</div>
+        </div>
+        <!---------------images-------------->
+
+	</div>
+</section>
+
+
+<!-------------------- Clients -------------------->
+<section class="pb-4 pt-5">
+	<div class="container">
+		<div class="d-flex flex-wrap justify-content-center justify-content-xl-between">
+			<a class="p-1" href="bbva"> 
+				<img 
+				src="<?php echo get_template_directory_uri() . '/assets/images/clients/'?>BBVA.png" 
+				class="img-fluid"
+				/>
+			</a>
+
+			<a class="p-1" href="artecola">
+				<img 
+				src="<?php echo get_template_directory_uri() . '/assets/images/clients/'?>Artecola.png" 
+				class="img-fluid"
+				/>
+			</a>
+
+			<a class="p-1" href="betfair">
+				<img 
+				src="<?php echo get_template_directory_uri() . '/assets/images/clients/'?>Betfair.png" 
+				class="img-fluid"
+				/>
+			</a>
+
+			<a class="p-1" href="bmatic">
+				<img 
+				src="<?php echo get_template_directory_uri() . '/assets/images/clients/'?>BMatic.png" 
+				class="img-fluid"
+				/>
+			</a>
+
+			<a class="p-1" href="aesa">
+				<img 
+				src="<?php echo get_template_directory_uri() . '/assets/images/clients/'?>AESA.png" 
+				class="img-fluid"
+				/>
+			</a>						
+		</div>
+	</div>
+</section>
+<!-------------------- Clients -------------------->
+
+<?php  get_footer(); ?>
---
+++ pages/betfair.php
@@ -0,0 +1,120 @@
+<?php 
+/* 
+    Template Name: BETFAIR
+*/
+get_header();
+
+$path_img = get_template_directory_uri() . '/assets/images/betfair/';
+?>
+
+<section class="pt-5 pb-3">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-6 text-center text-md-left">
+				<img
+					src="<?php echo $path_img?>title.svg"
+					class="img-fluid px-1 py-3"
+				/>
+				<h1 class="px-1 py-2 text-light-gray title-client text-uppercase">
+					Juegos de azar online
+				</h1>
+			</div>
+			<div class="col-md-6">
+				<div class="p-2 text-light-gray text-main-client">
+					Una empresa de juegos de azar en línea que opera el mayor intercambio de apuestas en línea del mundo.<br>Realizamos toda la gestión y diseño de su presentación para la Conmebol en Lima - Perú.
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="bg-light-gray pt-3 pb-5">
+	<div class="container">
+		<!---------------title--------------->
+		<div class="container-subtitle-client">
+			<h2 class="text-red text-center subtitle-client p-1 m-0">Gestión de evento - activación<br>Conmebol</h2>
+
+			<p class="text-light-gray text-center p-1 m-0 text-secondary-client">
+				Realizamos el diseño y gestión para el evento Conmebol en Lima - Perú para nuestro cliente Betfair.
+			</p>
+		</div>
+		<!---------------title--------------->
+
+		<!---------------images-------------->
+		<div class="row">
+			<div class="col-12 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Evento-Betfair.jpg" class="w-100 h-100" />
+			</div>
+			<div class="col-md-4 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Fotografía-vertical-1.jpg" class="w-100 h-100" />
+			</div>
+			<div class="col-md-4 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Fotografía-vertical-2.jpg" class="w-100 h-100" />
+			</div>                        
+			<div class="col-md-4 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Fotografía-vertical-3.jpg" class="w-100 h-100" />
+			</div>
+            <div class="col-md-6 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Activación-Betfair-2.jpg" class="w-100 h-100" />
+			</div>
+			<div class="col-md-6 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Activación-Betfair-1.jpg" class="w-100 h-100" />
+			</div>
+			<div class="col-md-4 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Fotografía-vertical-6.jpg" class="w-100 h-100" />
+			</div>                        
+			<div class="col-md-4 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Fotografía-vertical-5.jpg" class="w-100 h-100" />
+			</div>
+            <div class="col-md-4 p-1 m-0 p-md-2">
+				<img src="<?php echo $path_img?>Fotografía-vertical-4.jpg" class="w-100 h-100" />
+			</div>
+        </div>
+        <!---------------images-------------->
+	</div>
+</section>
+
+<!-------------------- Clients -------------------->
+<section class="pb-4 pt-5">
+	<div class="container">
+		<div class="d-flex flex-wrap justify-content-center justify-content-xl-between">
+			<a class="p-1" href="bbva"> 
+				<img 
+				src="<?php echo get_template_directory_uri() . '/assets/images/clients/'?>BBVA.png" 
+				class="img-fluid"
+				/>
+			</a>
+
+			<a class="p-1" href="artecola">
+				<img 
+				src="<?php echo get_template_directory_uri() . '/assets/images/clients/'?>Artecola.png" 
+				class="img-fluid"
+				/>
+			</a>
+
+			<a class="p-1" href="betfair">
+				<img 
+				src="<?php echo get_template_directory_uri() . '/assets/images/clients/'?>Betfair.png" 
+				class="img-fluid"
+				/>
+			</a>
+
+			<a class="p-1" href="bmatic">
+				<img 
+				src="<?php echo get_template_directory_uri() . '/assets/images/clients/'?>BMatic.png" 
+				class="img-fluid"
+				/>
+			</a>
+
+			<a class="p-1" href="aesa">
+				<img 
+				src="<?php echo get_template_directory_uri() . '/assets/images/clients/'?>AESA.png" 
+				class="img-fluid"
+				/>
+			</a>						
+		</div>
+	</div>
+</section>
+<!-------------------- Clients -------------------->
+
+<?php  get_footer(); ?>
