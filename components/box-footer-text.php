dc556a68df95a071b814862a618c63843fe1371c
programador <programador@MacBook-Air-de-programador.local>
2022-03-30 07:49:45 -0500

    datos guardados

---
+++ components/box-footer-text.php
@@ -0,0 +1,9 @@
+<style>
+  .width-12 {
+    width: 12.4rem;
+  }
+</style>
+<div class="mt-5">
+  <h2 class="text-light font-weight-bold" style="font-size: 24.17px;"><?php echo $args['title'] ?></h2>
+  <p class="text-light width-12"><i><?php echo $args['description'] ?></i></p>
+</div>
