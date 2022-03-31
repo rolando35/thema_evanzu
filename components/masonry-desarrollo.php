2e4fe06fc623d470f60c1e5f83318a39dba0ec2f
rolando <rolando@evanzu.com>
2022-03-31 14:57:31 -0500

    mansonry develop

---
+++ components/masonry-desarrollo.php
@@ -0,0 +1,51 @@
+<div class="container">
+  <div class="d-md-flex justify-content-center flex-md-row mb-2">
+    <div class="">
+      <div class="m-1">
+          <img src="<?php echo get_option("desarrollo")["galleri-image-five"]; ?>">
+      </div>
+      <div
+        class="d-md-flex justify-content-center flex-md-row"
+      >
+        <div class="m1"><img src="<?php echo get_option("desarrollo")["galleri-image-six"]; ?>"></div>
+        <div class="">
+          <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-seven"]; ?>"></div>
+          <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-eight"]; ?>"></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="">
+      <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-one"]; ?>"></div>
+
+      <div class="d-md-flex justify-content-center flex-md-row">
+        
+        <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-two"]; ?>"></div>
+        <div class="">
+          <div class="mt-1"><img src="<?php echo get_option("desarrollo")["galleri-image-three"]; ?>"></div>
+          <div class="mt-3 ml-1 d-sm-none d-md-block"><img src="<?php echo get_option("desarrollo")["galleri-image-four"]; ?>"></div>
+        </div>
+      </div>
+
+      <div class="d-md-flex justify-content-center  flex-md-row">
+        <div>
+                <img src="<?php echo get_option("desarrollo")["galleri-image-nine-a"]; ?>">
+        </div>
+         <div class="ml-1 d-sm-none d-md-block d-none px-1 pb-1">
+                <img src="<?php echo get_option("desarrollo")["galleri-image-nine-b"]; ?>">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="d-md-flex justify-content-center flex-md-row mb-3">
+    <div>
+          <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-teen"]; ?>"></div>
+          <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-eleven"]; ?>"></div>
+    </div>
+   <div>
+         <div class="m-1 "><img src="<?php echo get_option("desarrollo")["galleri-image-twelve"]; ?>"></div>
+   </div>
+   
+  </div>
+</div>
