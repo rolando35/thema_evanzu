40213741121fb794ee5410df95513201e500a49d
programador <programador@MacBook-Air-de-programador.local>
2022-04-21 11:35:43 -0500

    giving thema hover to masonry home evanzu

--- components/masonry-home.php
+++ components/masonry-home.php
@@ -1,16 +1,75 @@
+<style>
+  .masonry-hover-lirycs{
+    display: none;
+  }
+
+  .hover-image:hover > div{
+    display: block !important;
+    z-index: 1;
+  }
+  .masonry-hover-lirycs:hover + img {
+    filter: brightness(20%) !important;
+  }
+</style>
+
 <div class="container">
   <div class="mb-1">
     <div class="d-md-flex justify-content-center flex-md-row text-center">
-      <div class="m-2"><img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option("home_main")["galleria-image-one"]; ?>"></div>
+      <div class="m-2 d-flex justify-content-center align-items-center hover-image">
+        <!-- Texts hover -->
+        <div class='position-absolute masonry-hover-lirycs'>
+          <h6 class="small text-light">-- ACTIVACIONES BTL --</h6>
+          <p class="text-light font-weight-bold" style='font-size: 3rem; line-height: 3rem;'>BBVA Continental</p>
+          <a class="text-red font-weight-bold fs-5" href="https://youtu.be/Y_xlvqJvI0k">https://youtu.be/Y_xlvqJvI0k</a>
+        </div>
+        <!-- Texts hover -->
+        <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option("home_main")["galleria-image-one"]; ?>">
+      </div>
        <div>
       <div class="d-md-flex  flex-md-row">
         <div class=" justify-content-center ">
-            <div class="m-1"><img src="<?php echo get_option("home_main")["galleria-image-two"]; ?>"></div>
-            <div class="m-1"><img src="<?php echo get_option("home_main")["galleria-image-four"]; ?>"></div>  
+            <!------------------------- Hover-image ------------------------->
+            <div class="m-1 d-flex justify-content-center align-items-center hover-image ">
+              <!-- Texts hover -->
+              <div class='position-absolute masonry-hover-lirycs'>
+                <h6 class="small text-light">-- ACTIVACIONES BTL --</h6>
+                <p class="text-light font-weight-bold" style='font-size: 24px; line-height: 10px;'>BBVA Continental</p>
+                <a class="text-red font-weight-bold fs-5" href="https://youtu.be/Y_xlvqJvI0k">https://youtu.be/Y_xlvqJvI0k</a>
+              </div>
+              <!-- Texts hover -->
+              <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option("home_main")["galleria-image-two"]; ?>">
+            </div>
+            <!------------------------- Hover-image ------------------------->
+            <div class="m-1 d-flex justify-content-center align-items-center hover-image">
+              <!-- Texts hover -->
+              <div class='position-absolute masonry-hover-lirycs'>
+                <h6 class="small text-light">-- ACTIVACIONES BTL --</h6>
+                <p class="text-light font-weight-bold " style='font-size: 2rem; line-height: 10px;'>BETFAIR</p>
+                <a class="text-red font-weight-bold fs-5" href="https://fb.watch/4_LpR8cZlY/">https://fb.watch/4_LpR8cZlY/</a>
+              </div>
+              <!-- Texts hover -->
+              <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option("home_main")["galleria-image-four"]; ?>">
+            </div>
         </div>
         <div class="justify-content-center">
-         <div class="m-1"><img src="<?php echo get_option("home_main")["galleria-image-three"]; ?>"></div>   
-          <div class="m-1"><img src="<?php echo get_option("home_main")["galleria-image-five"]; ?>"></div>   
+          <div class="m-1 d-flex justify-content-center align-items-center hover-image">
+            <!-- Texts hover -->
+            <div class='position-absolute masonry-hover-lirycs'>
+              <p class=" text-light font-weight-bold " style='font-size: 2rem; line-height: 10px;'>Tambo</p>
+              <h6 class="text-red font-weight-bold fs-5">Video Pixel art</h6>
+            </div>
+            <!-- Texts hover -->
+            <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option("home_main")["galleria-image-three"]; ?>">
+          </div>   
+          <div class="m-1 d-flex justify-content-center align-items-center hover-image">
+            <!-- Texts hover -->
+            <div class='position-absolute masonry-hover-lirycs'>
+              <p class=" text-light font-weight-bold " style='font-size: 2rem; line-height: 10px;'>AESA</p>
+              <h6 class="text-red font-weight-bold fs-5">DISEÑO GRÁFICO</h6>
+            </div>
+            <!-- Texts hover -->
+            <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option("home_main")["galleria-image-five"]; ?>">
+          </div>   
         </div>
       </div>
       </div>
@@ -18,22 +77,88 @@
 
     <div class="d-md-flex justify-content-center flex-md-row text-center">   
         
-          <div class="m-1"><img src="<?php echo get_option("home_main")["galleria-image-six"]; ?>"></div>
-          <div class="m-1"><img src="<?php echo get_option("home_main")["galleria-image-seven"]; ?>"></div>
-          <div class="m-1"><img class="responsive-masonry-image" src="<?php echo get_option("home_main")["galleria-image-eight"] ?>"></div>
+          <div class="m-1 d-flex justify-content-center align-items-center hover-image">
+            <!-- Texts hover -->
+            <div class='position-absolute masonry-hover-lirycs'>
+              <p class=" text-light font-weight-bold " style='font-size: 2rem; line-height: 10px;'>BMatic</p>
+              <h6 class="text-red font-weight-bold fs-5">DESARROLLO WEB</h6>
+            </div>
+            <!-- Texts hover -->
+            <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option("home_main")["galleria-image-six"]; ?>">
+          </div>
+          <div class="m-1 d-flex justify-content-center align-items-center hover-image">
+            <!-- Texts hover -->
+            <div class='position-absolute masonry-hover-lirycs'>
+              <p class=" text-light font-weight-bold " style='font-size: 2rem; line-height: 10px;'>Tambo</p>
+              <h6 class="text-red font-weight-bold fs-5">DISEÑO GRÁFICO</h6>
+            </div>
+            <!-- Texts hover -->
+            <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option("home_main")["galleria-image-seven"]; ?>">
+          </div>
+          <div class="m-1 d-flex justify-content-center align-items-center hover-image">
+            <!-- Texts hover -->
+            <div class='position-absolute masonry-hover-lirycs'>
+              <p class=" text-light font-weight-bold " style='font-size: 2rem; line-height: 10px;'>Atelier 570</p>
+              <h6 class="text-red font-weight-bold fs-5">DESARROLLO WEB</h6>
+            </div>
+            <!-- Texts hover -->
+            <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option("home_main")["galleria-image-eight"] ?>">
+          </div>
        
      </div>
 
     <div class="d-md-flex justify-content-center flex-md-row">
-      <div class="text-center"><img src="<?php echo get_option("home_main")["galleria-image-nine"]; ?>"></div>
+      <div class="text-center d-flex justify-content-center align-items-center hover-image">
+        <!-- Texts hover -->
+            <div class='position-absolute masonry-hover-lirycs'>
+            <h6 class="small text-light">-- ACTIVACIONES BTL --</h6>
+            <p class=" text-light font-weight-bold " style='font-size: 2rem; line-height: 10px;'>AESA</p>
+            <a class="text-red font-weight-bold fs-5" href="https://fb.watch/50aZyUgqKt/">https://fb.watch/50aZyUgqKt/</a>
+            </div>
+            <!-- Texts hover -->
+        <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option("home_main")["galleria-image-nine"]; ?>">
+      </div>
       <div class="text-center">
           <div class="d-md-flex justify-content-center flex-md-row">
-            <div class="m-1"><img src="<?php echo get_option("home_main")["galleria-image-teen"]; ?>"></div>
-            <div class="m-1"><img src="<?php echo get_option("home_main")["galleria-image-eleven"]; ?>"></div>
+            <div class="m-1 d-flex justify-content-center align-items-center hover-image">
+              <!-- Texts hover -->
+            <div class='position-absolute masonry-hover-lirycs'>
+              <p class=" text-light font-weight-bold " style='font-size: 2rem; line-height: 10px;'>BIM</p>
+              <h6 class="text-red font-weight-bold fs-5">DISEÑO GRÁFICO</h6>
+            </div>
+            <!-- Texts hover -->
+              <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option("home_main")["galleria-image-teen"]; ?>">
+            </div>
+            <div class="m-1 d-flex justify-content-center align-items-center hover-image">
+              <!-- Texts hover -->
+            <div class='position-absolute masonry-hover-lirycs'>
+              <p class=" text-light font-weight-bold " style='font-size: 1.7rem; line-height: 10px;'>Adventure Life</p>
+              <h6 class="text-red font-weight-bold fs-5">BRANDING - DISEÑO GRÁFICO</h6>
+            </div>
+            <!-- Texts hover -->
+              <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option("home_main")["galleria-image-eleven"]; ?>">
+            </div>
           </div>     
-        <div class="m-2"><img class="responsive-masonry-image" src="<?php echo get_option("home_main")["galleria-image-twelve"]; ?>"></div>
+        <div class="m-2 d-flex justify-content-center align-items-center hover-image">
+          <!-- Texts hover -->
+            <div class='position-absolute masonry-hover-lirycs'>
+              <h6 class="small text-light">-- PRODUCCIÓN AUDIOVISUAL --</h6>
+              <p class=" text-light font-weight-bold " style='font-size: 2rem; line-height: 10px;'>Caja Huancayo</p>
+              <a class="text-red font-weight-bold fs-5" href="https://youtu.be/_l2wKC9ECdk">https://youtu.be/_l2wKC9ECdk</a>
+            </div>
+            <!-- Texts hover -->
+          <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option("home_main")["galleria-image-twelve"]; ?>">
+        </div>
       </div>
-      <div class="m-2 text-center"><img src="<?php echo get_option("home_main")["galleria-image-thirteen"]; ?>"></div>
+      <div class="m-2 text-center d-flex justify-content-center align-items-center hover-image">
+        <!-- Texts hover -->
+        <div class='position-absolute masonry-hover-lirycs'>
+          <p class=" text-light font-weight-bold " style='font-size: 2rem; line-height: 10px;'>Servir</p>
+          <h6 class="text-red font-weight-bold fs-5">MARKETING DIGITAL</h6>
+        </div>
+        <!-- Texts hover -->
+        <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option("home_main")["galleria-image-thirteen"]; ?>">
+      </div>
     </div>
   </div>
 
