bb9831386529293caaf1387dbf0e558eb791e9a7
rolando <rolando@evanzu.com>
2022-04-21 13:42:34 -0500

    btl refixed

--- components/masonry-desarrollo.php
+++ components/masonry-desarrollo.php
@@ -2,37 +2,37 @@
   <div class="d-md-flex justify-content-center flex-md-row mb-2">
     <div class="">
       <div class="m-1">
-          <img src="<?php echo get_option("desarrollo")["galleri-image-five"]; ?>">
+          <img src="<?php echo get_option("desarrollo")["galleri-image-five-dev"]; ?>">
       </div>
       <div
         class="d-md-flex justify-content-center flex-md-row"
       >
-        <div class="m1"><img src="<?php echo get_option("desarrollo")["galleri-image-six"]; ?>"></div>
+        <div class="m1"><img src="<?php echo get_option("desarrollo")["galleri-image-six-dev"]; ?>"></div>
         <div class="">
-          <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-seven"]; ?>"></div>
-          <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-eight"]; ?>"></div>
+          <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-seven-dev"]; ?>"></div>
+          <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-eight-dev"]; ?>"></div>
         </div>
       </div>
     </div>
 
     <div class="">
-      <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-one"]; ?>"></div>
+      <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-one-dev"]; ?>"></div>
 
       <div class="d-md-flex justify-content-center flex-md-row">
-        
-        <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-two"]; ?>"></div>
+           <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-two-dev"]; ?>"></div>
+    
         <div class="">
-          <div class="mt-1"><img src="<?php echo get_option("desarrollo")["galleri-image-three"]; ?>"></div>
-          <div class="mt-3 ml-1 d-sm-none d-md-block"><img src="<?php echo get_option("desarrollo")["galleri-image-four"]; ?>"></div>
+          <div class="mt-1"><img src="<?php echo get_option("desarrollo")["galleri-image-three-dev"]; ?>"></div>
+          <div class="mt-3 ml-1 d-sm-none d-md-block"><img src="<?php echo get_option("desarrollo")["galleri-image-four-dev"]; ?>"></div>
         </div>
       </div>
 
       <div class="d-md-flex justify-content-center  flex-md-row">
         <div>
-                <img src="<?php echo get_option("desarrollo")["galleri-image-nine-a"]; ?>">
+                <img src="<?php echo get_option("desarrollo")["galleri-image-nine-a-hover"]; ?>">
         </div>
          <div class="ml-1 d-sm-none d-md-block d-none px-1 pb-1">
-                <img src="<?php echo get_option("desarrollo")["galleri-image-nine-b"]; ?>">
+                <img src="<?php echo get_option("desarrollo")["galleri-image-nine-b-dev"]; ?>">
         </div>
       </div>
     </div>
@@ -40,11 +40,11 @@
 
   <div class="d-md-flex justify-content-center flex-md-row mb-3">
     <div>
-          <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-teen"]; ?>"></div>
-          <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-eleven"]; ?>"></div>
+          <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-teen-dev"]; ?>"></div>
+          <div class="m-1"><img src="<?php echo get_option("desarrollo")["galleri-image-eleven-dev"]; ?>"></div>
     </div>
    <div>
-         <div class="m-1 "><img src="<?php echo get_option("desarrollo")["galleri-image-twelve"]; ?>"></div>
+         <div class="m-1 "><img src="<?php echo get_option("desarrollo")["galleri-image-twelve-dev"]; ?>"></div>
    </div>
    
   </div>
--- components/masonry-diseño.php
+++ components/masonry-diseño.php
@@ -5,23 +5,23 @@
     <div class="m-1">
           <div ></div>
           <img class="toggle" src="<?php echo get_option("branding")["galleri-image-five"]; ?>">
-          <img class="toggle-position" src="<?php echo get_option("branding_hover")["galleri-image-five"]; ?>">
+         
       </div>
       <div
         class="d-md-flex justify-content-center flex-md-row  mb-2"
       >
         <div class="m1">
             <img class="toggle" src="<?php echo get_option("branding")["galleri-image-six"]; ?>">
-            <img class="toggle-position" src="<?php echo get_option("branding_hover")["galleri-image-six"]; ?>">
+           
         </div>
         <div class="">
           <div class="m-1">
               <img class="toggle" src="<?php echo get_option("branding")["galleri-image-seven"]; ?>">
-              <img class="toggle-position" src="<?php echo get_option("branding_hover")["galleri-image-seven"]; ?>">
+             
           </div>
           <div class="m-1">
               <img class="toggle" src="<?php echo get_option("branding")["galleri-image-eight"]; ?>">
-              <img class="toggle-position" src="<?php echo get_option("branding_hover")["galleri-image-eight"]; ?>">
+            
           </div>
         </div>
       </div>
@@ -30,30 +30,30 @@
     <div class="">
       <div class="m-1 position-relative">
         <img class="toggle" src="<?php echo get_option("branding")["galleri-image-one"]; ?>">
-        <img class="toggle-position" src="<?php echo get_option("branding_hover")["galleri-image-one"]; ?>">
+      
       </div>
 
       <div class="d-md-flex justify-content-center flex-md-row">
         
         <div class="m-1">
           <img class="toggle" src="<?php echo get_option("branding")["galleri-image-two"]; ?>">
-          <img class="toggle-position" src="<?php echo get_option("branding_hover")["galleri-image-two"]; ?>">
+         
         </div>
         <div class="">
           <div class="m-1">
             <img class="toggle" src="<?php echo get_option("branding")["galleri-image-three"]; ?>">
-            <img class="toggle-position" src="<?php echo get_option("branding_hover")["galleri-image-three"]; ?>">
+           
         </div>
           <div class="m-1">
             <img class="toggle" src="<?php echo get_option("branding")["galleri-image-four"]; ?>">
-            <img class="toggle-position" src="<?php echo get_option("branding_hover")["galleri-image-four"]; ?>">
+           
         </div>
         </div>
       </div>
 
       <div style="background-image:url()" class="m-1">
         <img class="toggle" src="<?php echo get_option("branding")["galleri-image-nine"]; ?>">
-        <img class="toggle-position" src="<?php echo get_option("branding_hover")["galleri-image-nine"]; ?>">
+     
       </div>
     </div>
   </div>
@@ -61,15 +61,15 @@
   <div class="d-md-flex justify-content-center flex-md-row b mb-3">
     <div class="m-1">
       <img class="toggle" src="<?php echo get_option("branding")["galleri-image-teen"]; ?>">
-      <img class="toggle-position" src="<?php echo get_option("branding_hover")["galleri-image-teen"]; ?>">
+    
       </div>
     <div class="m-1">
       <img class="toggle" src="<?php echo get_option("branding")["galleri-image-eleven"]; ?>">
-      <img class="toggle-position" src="<?php echo get_option("branding_hover")["galleri-image-eleven"]; ?>">
+     
       </div>
     <div class="m-1 ">
       <img src="<?php echo get_option("branding")["galleri-image-twelve"]; ?>">
-      <!-- <img class="toggle-position" src="<?php //echo get_option("branding_hover")["galleri-image-twelve"]; ?>"> -->
+   
     </div>
   </div>
 </div>
