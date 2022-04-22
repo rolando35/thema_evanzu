936730280a6a4ebb8b7e1231d2bbe675a57f25de
programador <programador@MacBook-Air-de-programador.local>
2022-04-22 17:37:25 -0500

    Finish hover effect to masonries

--- components/masonry-marketing.php
+++ components/masonry-marketing.php
@@ -1,30 +1,120 @@
 <div class="container">
   <div class="mb-1">
     <div class="d-md-flex justify-content-center flex-md-row">
-      <div class="m-2"><img src="<?php echo get_option("marketing")["galeria-image-one"]; ?>"></div>
+      <div class="m-2 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
+        <!-- Texts hover -->
+          <div class='position-absolute masonry-hover-lirycs ml-4'>
+            <h6 class="small text-red fs-5 font-weight-bold">CAMPAÑA DE EMAILING</h6>
+            <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>BBVA Continental</p>
+          </div>
+          <!-- Texts hover -->
+      <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option('marketing')['galeria-image-one']; ?>">
+    </div>
 
       <div class="">
-        <div class="m-1"><img src="<?php echo get_option("marketing")["galeria-image-two"]; ?>"></div>
+        <div class="m-1 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
+          <!-- Texts hover -->
+          <div class='position-absolute masonry-hover-lirycs ml-4'>
+            <h6 class="small text-red fs-5 font-weight-bold">GESTIÓN DE REDES SOCIALES</h6>
+            <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>Ciro Gálvez</p>
+          </div>
+          <!-- Texts hover -->
+        <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option('marketing')['galeria-image-two']; ?>">
+      </div>
         <div class="d-md-flex justify-content-center flex-md-row">
-          <div class="m-1"><img src="<?php echo get_option("marketing")["galeria-image-three"]; ?>"></div>
-          <div class="m-1"><img src="<?php echo get_option("marketing")["galeria-image-four"]; ?>"></div>
+          <div class="m-1 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
+            <!-- Texts hover -->
+          <div class='position-absolute masonry-hover-lirycs ml-4'>
+            <h6 class="small text-red fs-5 font-weight-bold">GESTIÓN DE REDES SOCIALES</h6>
+            <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>SERVIR</p>
+          </div>
+          <!-- Texts hover -->
+          <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option('marketing')[
+              'galeria-image-three'
+          ]; ?>">
+        </div>
+          <div class="m-1 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
+            <!-- Texts hover -->
+          <div class='position-absolute masonry-hover-lirycs ml-4'>
+            <h6 class="small text-red fs-5 font-weight-bold">GESTIÓN DE REDES SOCIALES</h6>
+            <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>Yakufarma</p>
+          </div>
+          <!-- Texts hover -->
+          <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option('marketing')[
+              'galeria-image-four'
+          ]; ?>">
+        </div>
           
         </div>
       </div>
     </div>
 
     <div class="d-md-flex justify-content-center flex-md-row">
-      <div class="bg-warning"><img src="<?php echo get_option("marketing")["galeria-image-five"]; ?>"></div>
+      <div class="bg-warning d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
+        <!-- Texts hover -->
+          <div class='position-absolute masonry-hover-lirycs ml-4'>
+            <h6 class="small text-red fs-5 font-weight-bold">GESTIÓN DE REDES SOCIALES</h6>
+            <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>BMatic</p>
+          </div>
+          <!-- Texts hover -->
+      <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option('marketing')['galeria-image-five']; ?>">
+    </div>
       <div>
           <div class="d-md-flex justify-content-center flex-md-row">
-            <div class="m-1"><img src="<?php echo get_option("marketing")["galeria-image-six"]; ?>"></div>
-            <div class="m-1"><img src="<?php echo get_option("marketing")["galeria-image-seven"]; ?>"></div>
+            <div class="m-1 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
+              <!-- Texts hover -->
+          <div class='position-absolute masonry-hover-lirycs ml-4'>
+            <h6 class="small text-red fs-5 font-weight-bold">EMAIL MARKETING</h6>
+            <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>Solintels</p>
+          </div>
+          <!-- Texts hover -->
+            <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option('marketing')[
+                'galeria-image-six'
+            ]; ?>">
+          </div>
+            <div class="m-1 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
+              <!-- Texts hover -->
+          <div class='position-absolute masonry-hover-lirycs ml-4'>
+            <h6 class="small text-red fs-5 font-weight-bold">GESTIÓN DE REDES SOCIALES</h6>
+            <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>SERVIR</p>
+          </div>
+          <!-- Texts hover -->
+            <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option('marketing')[
+                'galeria-image-seven'
+            ]; ?>">
+          </div>
           </div>     
-        <div class="m-2"><img class="img-fluid" src="<?php echo get_option("marketing")["galeria-image-eight"]; ?>"></div>
+        <div class="m-2 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
+          <!-- Texts hover -->
+          <div class='position-absolute masonry-hover-lirycs ml-4'>
+            <h6 class="small text-red fs-5 font-weight-bold">GOOGLE ADWORDS</h6>
+            <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>Evanzu</p>
+          </div>
+          <!-- Texts hover -->
+        <img class="img-fluid responsive-masonry-image hover-masonry-image" src="<?php echo get_option('marketing')[
+            'galeria-image-eight'
+        ]; ?>">
       </div>
-      <div class="m-2"><img src="<?php echo get_option("marketing")["galeria-image-nine"]; ?>"></div>
+      </div>
+      <div class="m-2 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
+        <!-- Texts hover -->
+          <div class='position-absolute masonry-hover-lirycs ml-4'>
+            <h6 class="small text-red fs-5 font-weight-bold">GOOGLE ADWORDS</h6>
+            <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>Evanzu</p>
+          </div>
+          <!-- Texts hover -->
+      <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option('marketing')['galeria-image-nine']; ?>">
+    </div>
     </div>
   </div>
 
-  <div class="d-flex justify-content-center m-2 mw-50"> <img src="<?php echo get_option("marketing")["galeria-image-then"]; ?>"></div>
+  <div class="d-flex justify-content-center m-2 mw-50 d-flex justify-content-center justify-content-lg-start align-items-end hover-image"> 
+    <!-- Texts hover -->
+      <div class='position-absolute masonry-hover-lirycs ml-4'>
+        <h6 class="small text-red fs-5 font-weight-bold">GESTIÓN DE REDES SOCIALES</h6>
+        <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>BBVA Continental</p>
+      </div>
+      <!-- Texts hover -->
+  <img class="responsive-masonry-image hover-masonry-image" src="<?php echo get_option('marketing')['galeria-image-then']; ?>">
+</div>
 </div>
