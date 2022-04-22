b8f5f8f20c5eb06b6f17f210eb0827a97740747b
programador <programador@MacBook-Air-de-programador.local>
2022-04-22 14:53:36 -0500

    Adding masonry to pages (Disenio, btl, production and home)

--- components/masonry-marketing.php
+++ components/masonry-marketing.php
@@ -1,30 +1,30 @@
 <div class="container">
   <div class="mb-1">
     <div class="d-md-flex justify-content-center flex-md-row">
-      <div class="m-2"><img src="<?php echo get_option("marketing_page")["galleri-image-one"]; ?>"></div>
+      <div class="m-2"><img src="<?php echo get_option("marketing")["galeria-image-one"]; ?>"></div>
 
       <div class="">
-        <div class="m-1"><img src="<?php echo get_option("marketing_page")["galleri-image-two"]; ?>"></div>
+        <div class="m-1"><img src="<?php echo get_option("marketing")["galeria-image-two"]; ?>"></div>
         <div class="d-md-flex justify-content-center flex-md-row">
-          <div class="m-1"><img src="<?php echo get_option("marketing_page")["galleri-image-three"]; ?>"></div>
-          <div class="m-1"><img src="<?php echo get_option("marketing_page")["galleri-image-four_new"]; ?>"></div>
+          <div class="m-1"><img src="<?php echo get_option("marketing")["galeria-image-three"]; ?>"></div>
+          <div class="m-1"><img src="<?php echo get_option("marketing")["galeria-image-four_new"]; ?>"></div>
           
         </div>
       </div>
     </div>
 
     <div class="d-md-flex justify-content-center flex-md-row">
-      <div class="bg-warning"><img src="<?php echo get_option("marketing_page")["galleri-image-five"]; ?>"></div>
+      <div class="bg-warning"><img src="<?php echo get_option("marketing")["galeria-image-five"]; ?>"></div>
       <div>
           <div class="d-md-flex justify-content-center flex-md-row">
-            <div class="m-1"><img src="<?php echo get_option("marketing_page")["galleri-image-six"]; ?>"></div>
-            <div class="m-1"><img src="<?php echo get_option("marketing_page")["galleri-image-seven"]; ?>"></div>
+            <div class="m-1"><img src="<?php echo get_option("marketing")["galeria-image-six"]; ?>"></div>
+            <div class="m-1"><img src="<?php echo get_option("marketing")["galeria-image-seven"]; ?>"></div>
           </div>     
-        <div class="m-2"><img class="img-fluid" src="<?php echo get_option("marketing_page")["galleri-image-eight"]; ?>"></div>
+        <div class="m-2"><img class="img-fluid" src="<?php echo get_option("marketing")["galeria-image-eight"]; ?>"></div>
       </div>
-      <div class="m-2"><img src="<?php echo get_option("marketing_page")["galleri-image-nine"]; ?>"></div>
+      <div class="m-2"><img src="<?php echo get_option("marketing")["galeria-image-nine"]; ?>"></div>
     </div>
   </div>
 
-  <div class="d-flex justify-content-center m-2 mw-50"> <img src="<?php echo get_option("marketing_page")["galleri-image-twelve"]; ?>"></div>
+  <div class="d-flex justify-content-center m-2 mw-50"> <img src="<?php echo get_option("marketing")["galeria-image-twelve"]; ?>"></div>
 </div>
