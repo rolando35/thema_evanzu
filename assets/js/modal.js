295f130a53dad4a3b8b8b46de5f90d5474052010
programador <programador@MacBook-Air-de-programador.local>
2022-04-25 09:44:22 -0500

    Add validation modal file

---
+++ assets/js/modal.js
@@ -0,0 +1,42 @@
+// let submit = document.getElementByClassName("submit");
+// console.log(submit);
+console.log("Hola perrito");
+
+// (function ($) {
+//   $(document).ready(function () {
+//     $("#basic-form").validate();
+//     $("button").click(function (event) {
+//       event.preventDefault();
+//       alert("This form will not submit");
+//     });
+//   });
+
+//   $(document).ready(function () {
+//     $("#basic-form").validate({
+//       rules: {
+//         name: {
+//           required: true,
+//           minlength: 3,
+//         },
+//         age: {
+//           required: true,
+//           number: true,
+//           min: 18,
+//         },
+//         email: {
+//           required: true,
+//           email: true,
+//         },
+//         weight: {
+//           required: {
+//             depends: function (elem) {
+//               return $("#age").val() > 50;
+//             },
+//           },
+//           number: true,
+//           min: 0,
+//         },
+//       },
+//     });
+//   });
+// })(jQuery);
