2991a3347fc2a88e19020e4eb0beb17ddf9bf2b3
rolando <rolando@evanzu.com>
2022-04-25 09:42:55 -0500

    formalario validation 2% advance

--- functions.php
+++ functions.php
@@ -212,22 +212,22 @@ function dcms_enviar_contenido()
    $message = "
     <html>
     	<head>
-<title>HTML email</title>
-</head>
-<body>
-<p>This email contains HTML Tags!</p>
-<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th> 
-</tr>
-<tr>
-<td>John</td>
-<td>Doe</td>
-</tr>
-</table>
-</body>
-</html>
+		<title>HTML email</title>
+		</head>
+		<body>
+		<p>This email contains HTML Tags!</p>
+		<table>
+		<tr>
+		<th>Firstname</th>
+		<th>Lastname</th> 
+		</tr>
+		<tr>
+		<td>John</td>
+		<td>Doe</td>
+		</tr>
+		</table>
+		</body>
+		</html>
 ";
 
 // Always set content-type when sending HTML email
