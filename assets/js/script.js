c207c17ae002c810eb916d0af9403b3f93692f94
rolando <rolando@evanzu.com>
2022-04-25 08:03:55 -0500

    sunday work mail

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+(function ($) {
+  alert("este texto est una prueba");
+  //   $("#send").on("click", function (e) {
+  //     e.preventDefault();
+  //     link = $(this);
+  //     id = link.attr("href").replace(/^.*#more-/, "");
+  //     $.ajax({
+  //       url: dcms_vars.ajaxurl,
+  //       type: "post",
+  //       data: {
+  //         action: "dcms_ajax_readmore",
+  //         id_post: id,
+  //       },
+  //       beforeSend: function () {
+  //         link.html("Cargando ...");
+  //       },
+  //       success: function (resultado) {
+  //         $("#post-" + id)
+  //           .find(".entry-content")
+  //           .html(resultado);
+  //       },
+  //     });
+  //   });
+})(jQuery);
