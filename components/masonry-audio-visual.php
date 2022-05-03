d8956aee823b2413194712f370b67dbbc4acafca
programador <programador@MacBook-Air-de-programador.local>
2022-05-03 10:01:11 -0500

    Add merge action to Rola branch

--- components/masonry-audio-visual.php
+++ components/masonry-audio-visual.php
@@ -5,7 +5,7 @@
             <div class="m-2 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
               <!-- Texts hover -->
               <div class='position-absolute masonry-hover-lirycs ml-4'>
-                <h6 class="small text-red fs-5 font-weight-bold">FOTOGRAFÍA DE ACTIVACIÓN</h6>
+                <h6 class="small text-red fs-12 font-weight-bold">FOTOGRAFÍA DE ACTIVACIÓN</h6>
                 <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>Agentes BBVA</p>
               </div>
               <!-- Texts hover -->
@@ -15,7 +15,7 @@
                     <div class="m-1 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
                       <!-- Texts hover -->
                       <div class='position-absolute masonry-hover-lirycs ml-4'>
-                        <h6 class="small text-red fs-5 font-weight-bold">FOTOGRAFÍAS PARA REDES</h6>
+                        <h6 class="small text-red fs-12 font-weight-bold">FOTOGRAFÍAS PARA REDES</h6>
                         <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>Yakufarma</p>
                       </div>
                       <!-- Texts hover -->
@@ -24,7 +24,7 @@
                       <div class="m-1 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
                       <!-- Texts hover -->
                       <div class='position-absolute masonry-hover-lirycs ml-4'>
-                        <h6 class="small text-red fs-5 font-weight-bold">FOTOGRAFÍAS DE ACTIVACIÓN</h6>
+                        <h6 class="small text-red fs-12 font-weight-bold">FOTOGRAFÍAS DE ACTIVACIÓN</h6>
                         <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>TDT</p>
                       </div>
                       <!-- Texts hover -->
@@ -34,7 +34,7 @@
             <div class="m-2 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
               <!-- Texts hover -->
               <div class='position-absolute masonry-hover-lirycs ml-4'>
-                <h6 class="small text-red fs-5 font-weight-bold">FOTOGRAFÍA DE EVENTO - FIN DE AÑOS</h6>
+                <h6 class="small text-red fs-12 font-weight-bold">FOTOGRAFÍA DE EVENTO - FIN DE AÑOS</h6>
                 <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>Agentes BBVA</p>
               </div>
               <!-- Texts hover -->
@@ -46,7 +46,7 @@
         <div class="m-1 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
           <!-- Texts hover -->
           <div class='position-absolute masonry-hover-lirycs ml-4'>
-            <h6 class="small text-red fs-5 font-weight-bold">FOTOGRAFÍA DE EVENTO</h6>
+            <h6 class="small text-red fs-12 font-weight-bold">FOTOGRAFÍA DE EVENTO</h6>
             <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>Aesa</p>
           </div>
           <!-- Texts hover -->
@@ -56,7 +56,7 @@
           <div class="m-1 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
               <!-- Texts hover -->
               <div class='position-absolute masonry-hover-lirycs ml-4'>
-                <h6 class="small text-red fs-5 font-weight-bold">FOTOGRAFÍA EN ESTUDIO</h6>
+                <h6 class="small text-red fs-12 font-weight-bold">FOTOGRAFÍA EN ESTUDIO</h6>
                 <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>Caja Huancayo</p>
               </div>
               <!-- Texts hover -->
@@ -68,7 +68,7 @@
               <div class="m-1 d-flex justify-content-center justify-content-lg-start align-items-end hover-image"> 
                 <!-- Texts hover -->
                 <div class='position-absolute masonry-hover-lirycs ml-4'>
-                  <h6 class="small text-red fs-5 font-weight-bold">FOTOGRAFÍAS DE PRODUCTOS</h6>
+                  <h6 class="small text-red fs-12 font-weight-bold">FOTOGRAFÍAS DE PRODUCTOS</h6>
                   <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>Yakufarma</p>
                 </div>
                 <!-- Texts hover -->
@@ -77,7 +77,7 @@
                 <div class="m-1 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
                 <!-- Texts hover -->
                 <div class='position-absolute masonry-hover-lirycs ml-4'>
-                  <h6 class="small text-red fs-5 font-weight-bold">FOTOGRAFÍA DE CAMPAÑA</h6>
+                  <h6 class="small text-red fs-12 font-weight-bold">FOTOGRAFÍA DE CAMPAÑA</h6>
                   <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>Agente BBVA</p>
                 </div>
                 <!-- Texts hover -->
@@ -88,7 +88,7 @@
           <div class="m-1 d-flex justify-content-center justify-content-lg-start align-items-end hover-image">
             <!-- Texts hover -->
             <div class='position-absolute masonry-hover-lirycs ml-4'>
-              <h6 class="small text-red fs-5 font-weight-bold">FOTOGRAFÍA EN ESTUDIO</h6>
+              <h6 class="small text-red fs-12 font-weight-bold">FOTOGRAFÍA EN ESTUDIO</h6>
               <p class="text-light font-weight-bold" style='font-size: 1.7rem; line-height: 3rem;'>Evanzu</p>
             </div>
             <!-- Texts hover -->
