71bcee2193a3fc5a253902800e86f6800c409553
rolando <rolando@evanzu.com>
2022-05-03 12:20:11 -0500

    advanced

--- pages/branding.php
+++ pages/branding.php
@@ -53,7 +53,6 @@ $path_img = get_template_directory_uri() . '/assets/images/btl/';
               class="align-top"
               src="<?php echo $directory_branding_imgs.'title_icon.svg'?>"
             />
-
             <br />
             DISEÑO
           </div>
@@ -112,370 +111,128 @@ $path_img = get_template_directory_uri() . '/assets/images/btl/';
       <div id="home" class="tab-pane active">
         <br />
         <div class="bg-light-gray">
-          <div class="big-title text-red my-4 text-center">Logo & Branding</div>
+          <div class="big-title text-red my-5 text-center">Logo & Branding</div>
           <div class="container">
-            <div class="row">
-              <div class="col-sm-6">
-                <img
-                  src="<?php echo $directory_branding_imgs ?>Identity Branding.png"
-                  alt=""
-                />
-                <div class="text-red text-center my-3">
-                  Ideal para promocionar
+             <div class="py-2">
+              <div class="container">
+                 <div class="row justify-content-center">
+                  <div class="col-md-11 col-lg-6 d-flex justify-content-lg-end">
+                    <img
+                      src="<?php echo $directory_branding_imgs ?>Identity Branding.png"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </div>
+                  <div class="col-md-10 col-lg-6">
+                    <div class="card bg-transparent border-0">
+                      <div class="card-body">
+                        <h3 class="subtitle-marketing text-light-purple text-left p-1">
+                        El branding de tu marca dejará huella
+                        </h3>
+                        <p class="text-justify text-light-gray p-1">
+                        <span class="font-weight-bold text-gray-black">Construimos marcas con personalidad y propósito.</span> 
+                           Tus clientes se llevarán una buena impresión, entenderán tus valores y  <span class="font-weight-bold text-gray-black"> amarán tu marca tanto como tú. </span> 
+                        </p>
+                        <p class="text-justify text-light-gray p-1">                       
+                            Trabajamos en el análisis y desarrollo total de una marca desde su concepto, 
+                            pronunciación, colores, formas y ventajas diferenciales, hasta la revisión del registro del nombre.
+                        </p>
+                      
+                      
+                      </div>
+                    </div>
+                  </div>
+                 </div>
+                       <div class="py-2 card-activation-2">
+              <div class="container">
+                <div  class="row mt-3">
+                  <div class="col-md-11 col-lg-6 py-2">
+                    <div class="right-conten-description">
+                        <div class="subtitle-marketing text-light-purple p-1">
+                           Tu marca cautivará y destacará 
+                        </div>
+                        <svg width="177" height="4" viewBox="0 0 177 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M3.49193 3.2865e-07L173.516 1.51926e-05C175.438 1.53607e-05 177 0.896557 177 2C177 3.10345 175.438 4 173.516 4L3.48442 3.99998C1.56198 3.99998 7.83793e-08 3.10343 1.74845e-07 1.99999C0.0075076 0.896542 1.56949 1.60585e-07 3.49193 3.2865e-07Z" fill="#FBC000"/>
+                        </svg>
+
+                    </div>
+                  </div>
+
+                  <div class="col-md-12 col-lg-6 py-5 left-conten-description">
+                    <img
+                      src="<?php echo $directory_branding_imgs ?>pincel_girl.png"
+                      class="img-fluid ml-4"
+                    />
+                  </div>
                 </div>
-                <p class="text-justify">
-                  Ventas, tráfico web, consideración de marca, productos,
-                  servicios, lanzamientos, ofertas, aplicaciones móviles y
-                  promociones. Sea cual sea tu objetivo publicitario (visitas a
-                  tu web, vistas de videos, conversiones, descargas de
-                  aplicaciones, visitas o llamadas a tu negocio físico, etc) te
-                  ayudaremos a alcanzarlo a través de los diversos formatos de
-                  publicidad.
-                </p>
-                <p class="text-justify">
-                  Da el primer paso para promocionar tus productos.
-                </p>
               </div>
-              <div class="col-sm-6">
-                <div class="my-3">
-                  Atrae a tu próximo comprador con publicidad en canales
-                  digitales.
-                </div>
-                <p class="text-justify">
-                  Atrae a tu próximo comprador con publicidad en canales
-                  digitales.
-                </p>
-                <div class="text-red my-1">
-                  Las plataformas digitales que manejamos:
-                </div>
-                <p>
-                  . <strong>Google Adwords</strong> (Búsqueda, Display, Youtube,
-                  Shopping y App móviles).
-                </p>
-                <p>
-                  .
-                  <strong>Publicidad en plataformas sociales</strong> (Facebook,
-                  Instagram, Twitter, Linkedin, TikTok, Wase, Spotify, Shazam, y
-                  otros).
-                </p>
-                <div class="text-center p-3">
-                  <img
-                    src="<?php echo $directory_branding_imgs ?>pincel_girl.png"
-                    alt=""
-                  />
-                </div>
+            </div>
+
               </div>
             </div>
           </div>
         </div>
-        <div>
-          <div class="big-title text-red my-4 text-center">
-            El servicio incluye:
-          </div>
-          <div>
-            <div class="d-flex justify-content-center">
-              <div>
-                <div class="mb-1">
-                  <div class="d-inline-block">
-                    <img
-                      src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/first-icon-service.svg"
-                    />
-                  </div>
-                  <div class="d-inline-block ml-1">
-                    Plan de medios digitales <br />
-                    (definir los canales con los <br />
-                    que tu público interactúa)
-                  </div>
+            
+            <div class="container">
+            <div class="d-flex flex-column align-items-center mb-n1-5">
+              <div class="d-flex flex-column flex-md-row">
+                <?php
+									get_template_part('components/btl/service-left','full', 
+										array(
+											'image' =>
+                  $directory_imgs."palabras-clabe.svg", 'text' => 'Manual de marca
+                          Plasmamos tu ADN corporativo en un documento guía para las diversas 
+                          formas de comunicación en función de tu negocio: Restaurantes, ONG’s, 
+                          Moda, Estudios Contables y Servicios.', 'line' => $directory_branding_imgs."line-red.svg" ) ); ?>
+                <div class="d-none d-lg-block container-center-service">
+                  <div class="element-center-service"></div>
                 </div>
-                <div class="ml-5">
-                  <img
-                    class="pl-2"
-                    src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/ponit-line-red.svg"
-                  />
-                </div>
+                <?php
+									get_template_part('components/btl/service-right','full', 
+										array(
+											'image' =>
+                $directory_imgs."meta-texto.svg", 'text' => '<strong
+                  >Implementar Etiquetas meta </strong>
+                    Descripción de las páginas que Google lee y posiciona.
+                ',
+                'line' => $directory_imgs."line-point-yellow.svg" ) ); ?>
               </div>
-              <div>
-                <div>
-                  <svg
-                    width="10"
-                    height="114"
-                    viewBox="0 0 7 56"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M0 3.5C0 1.567 1.567 0 3.5 0C5.433 0 7 1.567 7 3.5V52.5C7 54.433 5.433 56 3.5 56C1.567 56 0 54.433 0 52.5V3.5Z"
-                      fill="#ED1164"
-                    />
-                  </svg>
-                </div>
-                <div>
-                  <svg
-                    width="19"
-                    height="19"
-                    viewBox="0 0 18 19"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <circle cx="9" cy="9.5" r="9" fill="#ED1164" />
-                  </svg>
-                </div>
-                <div>
-                  <svg
-                    width="18"
-                    height="19"
-                    viewBox="0 0 18 19"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <circle
-                      r="9"
-                      transform="matrix(-1 0 0 1 9 9.5)"
-                      fill="#FBC000"
-                    />
-                  </svg>
-                </div>
-              </div>
-              <div>
-                <div class="mb-1 mt-5">
-                  <div class="d-inline-block ml-1">
-                    Definir el <strong>formato </strong> <br />
-                    de <strong>publicidad</strong> adecuado.
-                  </div>
-                  <div class="d-inline-block ml-4">
-                    <img
-                      src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/secon-icon-righ.svg"
-                    />
-                  </div>
-                </div>
 
-                <div>
-                  <svg
-                    width="290"
-                    height="23"
-                    viewBox="0 0 290 26"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M0.999998 24.5L288 24.5C288.552 24.5 289 24.0523 289 23.5L289 1"
-                      stroke="#FBC000"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-dasharray="6 6"
-                    />
-                  </svg>
-                </div>
+              <div class="d-flex flex-column flex-md-row">
+                <?php
+									get_template_part('components/btl/service-left','full', 
+										array(
+											'image' =>
+                $directory_imgs."factores-internos.svg", 'text' => '<strong
+                  >Optimizar factores internos</strong>
+                    Contenido web, sistemas y otros.
+              
+                <br />
+                   Banners, gifs, micro videos, spots publicitarios y otros.', 'line' =>
+                $path_img."line-rosado.svg" ) ); ?>
+                <div class="d-none d-lg-block container-center-service"></div>
+                <?php
+									get_template_part('components/btl/service-right','full', 
+										array(
+											'image' =>
+                  $directory_imgs."indexacion-google.svg", 'text' => '<strong
+                  >Indexación en Google</strong
+                >
+                Para que Google muestre el índice de tu web en los resultados de búsqueda.', 'line' => $directory_imgs."line-point-yellow.svg" ) ); ?>
               </div>
-            </div>
 
-            <div class="d-flex justify-content-center">
-              <div>
-                <div class="mb-1">
-                  <div class="d-inline-block">
-                    <img
-                      src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/contain-define.svg"
-                    />
-                  </div>
-                  <div class="d-inline-block ml-1">
-                    <strong>
-                      Definición de contenidos <br />
-                      publicitarios</strong
-                    >
-                    (Mensaje y <br />
-                    gráfica o audiovisual).
-                  </div>
-                </div>
-                <div class="ml-5">
-                  <img
-                    class="pl-2"
-                    src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/ponit-line-red.svg"
-                  />
-                </div>
-              </div>
-              <div>
-                <div>
-                  <svg
-                    width="10"
-                    height="114"
-                    viewBox="0 0 7 56"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M0 3.5C0 1.567 1.567 0 3.5 0C5.433 0 7 1.567 7 3.5V52.5C7 54.433 5.433 56 3.5 56C1.567 56 0 54.433 0 52.5V3.5Z"
-                      fill="#ED1164"
-                    />
-                  </svg>
-                </div>
-                <div>
-                  <svg
-                    width="19"
-                    height="19"
-                    viewBox="0 0 18 19"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <circle cx="9" cy="9.5" r="9" fill="#ED1164" />
-                  </svg>
-                </div>
-                <div>
-                  <svg
-                    width="18"
-                    height="19"
-                    viewBox="0 0 18 19"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <circle
-                      r="9"
-                      transform="matrix(-1 0 0 1 9 9.5)"
-                      fill="#FBC000"
-                    />
-                  </svg>
-                </div>
-              </div>
-              <div>
-                <div class="mb-1 mt-3">
-                  <div class="d-inline-block ml-1">
-                    <strong
-                      >Despliegue y configuración <br />
-                      de la campaña en la
-                    </strong>
-                    <br />plataforma seleccionada.
-                  </div>
-                  <div class="d-inline-block">
-                    <img
-                      src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/deploy-configuration.svg"
-                    />
-                  </div>
-                </div>
-
-                <div>
-                  <svg
-                    width="290"
-                    height="24"
-                    viewBox="0 0 290 26"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M0.999998 24.5L288 24.5C288.552 24.5 289 24.0523 289 23.5L289 1"
-                      stroke="#FBC000"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-dasharray="6 6"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-
-            <div class="d-flex justify-content-center">
-              <div>
-                <div class="mb-1">
-                  <div class="d-inline-block">
-                    <img
-                      src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/rocket-optimisation.svg"
-                    />
-                  </div>
-                  <div class="d-inline-block ml-2">
-                    <strong>Optimización</strong> de la <br />campaña.
-                  </div>
-                </div>
-                <div class="ml-5">
-                  <img
-                    class="pl-2"
-                    src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/ponit-line-red.svg"
-                  />
-                </div>
-              </div>
-              <div>
-                <div>
-                  <svg
-                    width="10"
-                    height="114"
-                    viewBox="0 0 7 56"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M0 3.5C0 1.567 1.567 0 3.5 0C5.433 0 7 1.567 7 3.5V52.5C7 54.433 5.433 56 3.5 56C1.567 56 0 54.433 0 52.5V3.5Z"
-                      fill="#ED1164"
-                    />
-                  </svg>
-                </div>
-                <div>
-                  <svg
-                    width="19"
-                    height="19"
-                    viewBox="0 0 18 19"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <circle cx="9" cy="9.5" r="9" fill="#ED1164" />
-                  </svg>
-                </div>
-                <div>
-                  <svg
-                    width="18"
-                    height="19"
-                    viewBox="0 0 18 19"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <circle
-                      r="9"
-                      transform="matrix(-1 0 0 1 9 9.5)"
-                      fill="#FBC000"
-                    />
-                  </svg>
-                </div>
-              </div>
-              <div>
-                <div class="ml-5 mt-5">
-                  <div class="d-inline-block ml-1 mr-5">
-                    <strong
-                      >Reportes <br />
-                      periódicos.</strong
-                    >
-                  </div>
-                  <div class="d-inline-block ml-5">
-                    <img
-                      src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/periodic-reports.svg"
-                    />
-                  </div>
-                </div>
-
-                <div>
-                  <svg
-                    width="290"
-                    height="24"
-                    viewBox="0 0 290 26"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M0.999998 24.5L288 24.5C288.552 24.5 289 24.0523 289 23.5L289 1"
-                      stroke="#FBC000"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-dasharray="6 6"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-
-            <div class="d-flex justify-content-center">
-              <div class="d-inline-block ml-5">
-                <img
-                  src="<?php echo get_template_directory_uri() ?>/assets/images/marketing-digital/root-tree.svg"
-                />
+              
+              <div class="d-none d-lg-flex">
+                <div
+                  class="container-center-service element-final-center"
+                ></div>
               </div>
             </div>
           </div>
-        </div>
+           
+
+
+
         <div class="bg-red py-5">
           <?php  
                                       $parameters = array ('svg' =>
