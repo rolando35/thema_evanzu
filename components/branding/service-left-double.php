f64aab06f859d7a76ba498aefe618302725531c3
rolando <rolando@evanzu.com>
2022-05-04 17:24:27 -0500

    brading

--- components/branding/service-left-double.php
+++ components/branding/service-left-double.php
@@ -1,7 +1,7 @@
-<div class="d-flex flex-column my-1 mt-lg-0 mb-lg-5">	
-	<div class="d-flex ">	
+<div class="d-flex flex-column my-1 mt-lg-0 mb-lg-5 text-center">	
+	<div class="d-md-flex">	
 		<div><img  src="<?php echo $args["image-one"]; ?>"	/><div class="text-center pt-1"> <?php echo $args["title-one"];?></div> </div>	 	
-		<div class="ml-5"> <img  src="<?php echo $args["image-two"]; ?>"/> <div class="text-center pt-1"><?php echo $args["title-two"];?></div> </div>
+		<div class="ml-md-5"> <img  src="<?php echo $args["image-two"]; ?>"/> <div class="text-center pt-1"><?php echo $args["title-two"];?></div> </div>
 	</div>	
 	<div class="d-none d-lg-flex justify-content-end container-line-left-service">
 		<div class="z-index-1">
