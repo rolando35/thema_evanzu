0b1caf4161cd505f49fa4bd95a96b9c21dd70230
AlexanderEvanzu <alexander@evanzu.com>
2022-05-09 15:48:53 -0500

    Ok

--- pages/marqueting_digital.php
+++ pages/marqueting_digital.php
@@ -469,7 +469,7 @@ $path_img = get_template_directory_uri() . '/assets/images/btl/';
             <?php get_template_part('components/form-quote-request2', 'full', [
                 'image' => $directory_imgs . 'form-quote-request.svg',
                 'title' => 'Define tu presupuesto',
-                'idwebsite' => 'marketing1website',
+                'idwebsite' => 'HomeWebsite',
                 'idemail' => 'marketing1email',
                 'idform' => 'marketing1',
                 'idparagraph' => 'pmarketing1',
@@ -666,7 +666,7 @@ $path_img = get_template_directory_uri() . '/assets/images/btl/';
                   [
                       'image' => $directory_imgs . 'form-quote-request.svg',
                       'title' => '¡Estás listo!',
-                      'idwebsite' => 'marketing2website',
+                      'idwebsite' => 'redes_socialesWebsite',
                       'idemail' => 'marketing2email',
                       'idform' => 'marketing2',
                       'idparagraph' => 'pmarketing2',
@@ -896,7 +896,7 @@ $path_img = get_template_directory_uri() . '/assets/images/btl/';
                   [
                       'image' => $directory_imgs . 'form-quote-request.svg',
                       'title' => 'Conoce tu calificación SEO',
-                      'idwebsite' => 'marketing3website',
+                      'idwebsite' => 'seoWebsite',
                       'idemail' => 'marketing3email',
                       'idform' => 'marketing3',
                       'idparagraph' => 'pmarketing3',
@@ -1068,7 +1068,7 @@ $path_img = get_template_directory_uri() . '/assets/images/btl/';
                   [
                       'image' => $directory_imgs . 'form-quote-request.svg',
                       'title' => 'Crece exponencialmente',
-                      'idwebsite' => 'marketing4website',
+                      'idwebsite' => 'menu3Website',
                       'idemail' => 'marketing4email',
                       'idform' => 'marketing4',
                       'idparagraph' => 'pmarketing4',
@@ -1291,7 +1291,7 @@ Haremos que todo lo que comuniques logre que un usuario <span class="hurme-bold-
             <?php get_template_part('components/form-quote-request2', 'full', [
                 'image' => $directory_imgs . 'form-quote-request.svg',
                 'title' => 'Tenemos más por contarte',
-                'idwebsite' => 'marketing5website',
+                'idwebsite' => 'menu4Website',
                 'idemail' => 'marketing5email',
                 'idform' => 'marketing5',
                 'idparagraph' => 'pmarketing5',
