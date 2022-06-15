c2c09bc850f078c60febe5a14551d26c8d0570b5
rolando <rolando@evanzu.com>
2022-06-15 17:51:29 -0500

    adavance evanzu

--- pages/blog.php
+++ pages/blog.php
@@ -2,20 +2,27 @@
 /* 
     Template Name: Blog
 */
-
+get_header();
  global $post;
 
 $last_posts = get_posts(array('posts_per_page' => 10));
-
+?>
+<div class="card-columns mt-5">
+<?php 
 foreach ( $last_posts as $post ) :
  setup_postdata( $post );?>
 
-<div class="last-posts-wrap">
- <?php the_post_thumbnail();?>
- <h3><?php the_title();?></h3>
- <p><?php the_excerpt();?></p>
-</div>
+ <!-- <p> <?php the_content();?> </p> -->
+  <div class="card">
+    <?php the_post_thumbnail();?>
+    <div class="card-body">
+      <h5 class="card-title"> <a href="<?php the_permalink(); ?>"><?php the_title();?></a> </h5>
+      <p class="card-text"><?php the_excerpt();?></p>
+    </div>
+  </div>
 
 <?php endforeach;
  wp_reset_postdata();
 ?>
+</div>
+<?php  get_footer(); ?>
