1c22a5c74dcee780b52bc4d7596eaca531b113b2
programador <programador@MacBook-Air-de-programador.local>
2022-06-16 14:19:55 -0500

    asdf

--- components/video-galery.php
+++ components/video-galery.php
@@ -150,7 +150,7 @@
     </div>
     <!-- Texts hover -->
       <!-- <video class="responsive-masonry-image hover-video-invert" controls muted id="myVideo">
-          <source src="<?php echo get_template_directory_uri() ?>/assets/video/_BBVA Continental - Stand de atención al cliente en la Feria Nexo Inmobiliario.mp4" type="video/mp4">
+          <source src="<?php  //echo get_template_directory_uri() ?>/assets/video/_BBVA Continental - Stand de atención al cliente en la Feria Nexo Inmobiliario.mp4" type="video/mp4">
       </video> -->
     </div>
   </div>
