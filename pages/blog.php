a754e53bf958d777481c1e413e8eb6347752fd5d
rolando <rolando@evanzu.com>
2022-06-16 13:15:54 -0500

    blog refine

--- pages/blog.php
+++ pages/blog.php
@@ -14,7 +14,7 @@ $last_posts = get_posts(array('posts_per_page' => 10));
     <!-- The slideshow -->
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img height="400" class="w-100" src=" <?php echo get_template_directory_uri(); ?>/assets/images/blog/Blog-banner.png">
+           <img height="400" class="w-100" src=" <?php echo get_template_directory_uri(); ?>/assets/images/blog/Blog-banner.png">
         
           	  <div class="carousel-caption mb-5 pb-5">
                   <h1 id="title-blog" class="hurme-bold-4 text-red" data-wow-delay="0.3s">BLOG</h1>     
@@ -22,7 +22,8 @@ $last_posts = get_posts(array('posts_per_page' => 10));
                     Obtén información sobre la industria, las últimas tendencias, conoce nuestro <br> proceso creativo y más
                   </div>
                    
-				</div>	
+              </div>
+        
         </div>
       
     </div>
@@ -32,15 +33,24 @@ $last_posts = get_posts(array('posts_per_page' => 10));
 <?php 
 foreach ( $last_posts as $post ) :
  setup_postdata( $post );?>
-
  <!-- <p> <?php the_content();?> </p> -->
   <div class="card">
-    <div class="text-center">
+    <div class="">
+        <div class="box-date-car" >     
+           <?php echo get_the_date();?>  I  	<?php the_category(); ?>
+        </div> 
          <?php the_post_thumbnail();?>
+
+          <div class="box-date-car" >
+              <?php the_author(); ?>
+        </div> 
     </div>
     <div class="card-body">
-      <h5 class="card-title"> <a href="<?php the_permalink(); ?>"><?php the_title();?></a> </h5>
+      <h5 class="card-title text-red font-weight-bold"> <?php the_title();?> </h5>
       <p class="card-text"><?php the_excerpt();?></p>
+      <div>
+             <a href="<?php the_permalink(); ?>"><span class="text-red font-weight-bold">Leer más </span> <img  src=" <?php echo get_template_directory_uri(); ?>/assets/images/blog/circle-more.svg"></a>
+      </div>  
     </div>
   </div>
 
