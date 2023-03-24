25f5c25860a681af6482a132b0d25580ca649f26
rolando <rolando@evanzu.com>
2023-03-24 15:18:28 -0500

    fixed styles builgin

--- components/form-quote-request2.php
+++ components/form-quote-request2.php
@@ -1,5 +1,20 @@
 <style>
 	p.error{ color: red; font-size: 0.8em; }
+	.form-controls-service {
+		display: block;
+		width: 100%;
+		height: calc(1.5em + 0.75rem + 2px);
+		padding: 0.375rem 0.75rem;
+		font-size: 1rem;
+		font-weight: 400;
+		line-height: 1.5;
+		color: #fff;
+		background-color: #fd73a7;
+		background-clip: padding-box;
+		border: 1px solid #ced4da;
+		border-radius: 0.25rem;
+		transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+		}
 </style>
 
 
